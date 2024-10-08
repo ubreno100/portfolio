7e86d6fa5c18d220b23299b21d98bb59b7b47dc5
Breno <breno.correa107@gmail.com>
2024-10-07 21:56:38 -0300

    escluindo arquivos desnecessarios

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <h1>Hello World</h1>
   </StrictMode>,
 )
